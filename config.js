5713c7ecf0393b3c37475835c08f02fe450922fe
Cibe-ofc <113320411+Cibe-ofc@users.noreply.github.com>
2022-11-11 08:28:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -17,14 +17,14 @@ global.APIKeys = {
 }
 
 
-global.ownername = 'THE JO BOT'
+global.ownername = 'King'
 global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6285797375014','6285797375014','6285797375014']
+global.premium = ['6285797375014']
+global.packname = 'king'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
